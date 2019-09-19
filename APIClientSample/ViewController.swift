cc81812a2a7a1d129d394ee38052a3435476d0a7
toshi-imaeda <toshi-imaeda@ixit.co.jp>
2019-09-19 22:43:16 +0900

    initial commit

---
+++ APIClientSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  APIClientSample
+//
+//  Created by 今枝 稔晴 on 2019/09/19.
+//  Copyright © 2019 iXIT Corporation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
